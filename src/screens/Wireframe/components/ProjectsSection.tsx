41771cc54a3ef64ffcef6cc89eee34ff4a25767b
MeruemShen <dorian1711x@gmail.com>
2025-07-19 21:50:27 +0200

    Reduct size element

--- src/screens/Wireframe/components/ProjectsSection.tsx
+++ src/screens/Wireframe/components/ProjectsSection.tsx
@@ -19,25 +19,25 @@ interface ProjectsSectionProps {
 }
 
 export const ProjectsSection = ({ projects }: ProjectsSectionProps): JSX.Element => (
-  <section id="work" className="absolute w-full top-[1744px] left-0 px-8">
-    <div className="relative mb-4 h-[48px] mt-[0px]">
+  <section id="work" className="absolute w-full top-[1744px] left-0 px-[26px]">
+    <div className="relative mb-4 h-[38px] mt-[0px]">
       {/* Texte centré en absolu */}
-      <h2 className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-[calc(50%+3.2px)] [text-shadow:0px_0px_24px_#000000bf] [font-family:'Days_One',Helvetica] font-normal text-[40px] text-center tracking-[0] leading-[39.6px] whitespace-nowrap text-white z-10">
+      <h2 className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-[calc(50%+2.6px)] [text-shadow:0px_0px_24px_#000000bf] [font-family:'Days_One',Helvetica] font-normal text-[32px] text-center tracking-[0] leading-[31.7px] whitespace-nowrap text-white z-10">
         <span>MES </span>
         <span className="text-[#a265ff]">PROJETS</span>
       </h2>
       {/* Trait */}
       <div className="flex justify-center items-center h-full">
-        <div className="ml-4 flex-grow h-[32px] flex items-center min-w-[48px]">
+        <div className="ml-[13px] flex-grow h-[26px] flex items-center min-w-[38px]">
           {/* Diamond */}
-          <div className="absolute left-1/2 translate-x-[192px] top-[calc(50%-1.6px)] -translate-y-1/2 w-[16px] h-[16px] bg-[#fff] rotate-45 z-0"></div>
+          <div className="absolute left-1/2 translate-x-[154px] top-[calc(50%-1.3px)] -translate-y-1/2 w-[13px] h-[13px] bg-[#fff] rotate-45 z-0"></div>
           {/* Line */}
-          <div className="absolute left-1/2 translate-x-[208px] right-[-19px] top-[calc(50%-1.6px)] -translate-y-1/2 h-[4px] bg-[#fff] z-0"></div>
+          <div className="absolute left-1/2 translate-x-[166px] right-[-15px] top-[calc(50%-1.3px)] -translate-y-1/2 h-[3px] bg-[#fff] z-0"></div>
         </div>
       </div>
     </div>
 
-    <div className="mt-[116px] grid grid-cols-1 md:grid-cols-7 gap-6 justify-items-center max-w-[1200px] mx-auto">
+    <div className="mt-[93px] grid grid-cols-1 md:grid-cols-7 gap-[19px] justify-items-center max-w-[960px] mx-auto">
       {projects.map((project, index) => {
         const row = Math.floor(index / 2)
         const isEvenRow = row % 2 === 0
@@ -48,36 +48,36 @@ export const ProjectsSection = ({ projects }: ProjectsSectionProps): JSX.Element
         return (
             <Card
               key={index}
-              className={`w-full h-[329px] bg-[#a265ff0d] rounded-[32px] border border-solid border-[#ffffff33] overflow-hidden ${colSpan}`}
+              className={`w-full h-[263px] bg-[#a265ff0d] rounded-[26px] border border-solid border-[#ffffff33] overflow-hidden ${colSpan}`}
             >
             <CardContent className="p-0 flex flex-col h-full">
-              <div className="w-full h-[122px] overflow-hidden p-[0.6rem]">
+              <div className="w-full h-[98px] overflow-hidden p-[0.48rem]">
                 <img className="w-full h-full object-cover rounded-[24px]" alt="Project Image" src={project.image} />
               </div>
-            <div className="flex flex-1 p-5 relative">
-              <div className="w-[18px] h-[145px] absolute left-[18px] rounded-[8px] border border-solid border-[#a265ff]" />
-              <div className="absolute w-[114px] top-[84px] left-[-29px] -rotate-90 [font-family:'Days_One',Helvetica] font-normal text-[#a265ff] text-[12px] text-center tracking-[0] leading-[12.7px]">
+            <div className="flex flex-1 p-4 relative">
+              <div className="w-[14px] h-[116px] absolute left-[14px] rounded-[8px] border border-solid border-[#a265ff]" />
+              <div className="absolute w-[91px] top-[67px] left-[-23px] -rotate-90 [font-family:'Days_One',Helvetica] font-normal text-[#a265ff] text-[10px] text-center tracking-[0] leading-[10px]">
                 {project.type}
               </div>
-              <div className="ml-10 relative w-[100%]">
-                <div className="flex items-center mb-3 relative">
-                  <h3 className="[font-family:'Days_One',Helvetica] font-normal text-white text-[19px] tracking-[0] leading-[19px] min-w-0 truncate">
+              <div className="ml-8 relative w-[100%]">
+                <div className="flex items-center mb-[10px] relative">
+                  <h3 className="[font-family:'Days_One',Helvetica] font-normal text-white text-[15px] tracking-[0] leading-[15px] min-w-0 truncate">
                     {project.title}
                   </h3>
-                  <div className="relative ml-4 flex-grow h-[32px] flex items-center min-w-[48px]">
+                  <div className="relative ml-[13px] flex-grow h-[26px] flex items-center min-w-[38px]">
                     {/* Diamond */}
-                    <div className="absolute left-0 top-[calc(50%-1.6px)] -translate-y-1/2 w-[16px] h-[16px] bg-[#a265ff] rotate-45 z-10"></div>
+                    <div className="absolute left-0 top-[calc(50%-1.3px)] -translate-y-1/2 w-[13px] h-[13px] bg-[#a265ff] rotate-45 z-10"></div>
                     {/* Line */}
-                    <div className="absolute left-[11px] right-[-19px] top-[calc(50%-1.6px)] -translate-y-1/2 h-[4px] bg-[#a265ff]"></div>
+                    <div className="absolute left-[9px] right-[-15px] top-[calc(50%-1.3px)] -translate-y-1/2 h-[3px] bg-[#a265ff]"></div>
                   </div>
                 </div>
-                <p className="[font-family:'Roboto',Helvetica] font-normal text-[#ffffffbf] text-[12px] tracking-[0] leading-[normal] mb-3">
+                <p className="[font-family:'Roboto',Helvetica] font-normal text-[#ffffffbf] text-[10px] tracking-[0] leading-[normal] mb-[10px]">
                   {project.description}
                 </p>
                 {project.technologies.length > 0 && (
-                  <div className="flex gap-3 mt-auto">
+                  <div className="flex gap-[10px] mt-auto">
                     {project.technologies.map((tech, techIndex) => (
-                      <img key={techIndex} className="w-6 h-6 object-cover" alt={tech.alt} src={tech.icon} />
+                      <img key={techIndex} className="w-5 h-5 object-cover" alt={tech.alt} src={tech.icon} />
                     ))}
                   </div>
                 )}
