09b40e19d3ff9603223cb3bd6b7d1f6530abe764
MeruemShen <dorian1711x@gmail.com>
2025-07-20 18:39:04 +0200

    Footer Arrow

--- src/screens/Wireframe/components/FooterSection.tsx
+++ src/screens/Wireframe/components/FooterSection.tsx
@@ -12,17 +12,21 @@ interface FooterSectionProps {
 }
 
 export const FooterSection = ({ footerLinks, socialLinks }: FooterSectionProps): JSX.Element => (
-  <footer className="absolute w-full h-[249px] top-[3100px] left-0 bg-[#170b28] border border-solid border-[#ffffff33] flex items-center justify-between px-12">
+  <footer className="absolute w-full h-[330px] top-[3100px] left-0 bg-[#170b28] border border-solid border-[#ffffff33] flex items-center justify-between px-12 pb-16">
     <div className="flex flex-col">
       <img className="h-[158px] w-[158px] object-cover mb-4" alt="Logo" src="/wireframe/logo.png" />
       <h3 className="[font-family:'Days_One',Helvetica] font-normal text-white text-[32px] tracking-[0] leading-[31.7px] mb-4">DeepBird</h3>
       <p className="[font-family:'Roboto',Helvetica] font-normal text-[#ffffffbf] text-[15px] tracking-[0] leading-[normal]">© 2024–2025 Deepbird. Tous droits réservés</p>
     </div>
-    <div className="flex gap-16">
-      <div className="relative">
-        <div className="w-[22px] h-[181px] absolute left-[-55px] rounded-[10px] border border-solid border-[#a265ff]" />
-        <div className="absolute w-[143px] top-[78px] left-[-113px] -rotate-90 [font-family:'Days_One',Helvetica] font-normal text-[#a265ff] text-base text-center tracking-[0] leading-[15.9px]">COACHING</div>
-        <ul className="flex flex-col gap-4">
+    <div className="flex gap-72">
+        <div className="relative">
+          <div className="w-[22px] h-[181px] absolute left-[-55px] rounded-[10px] border border-solid border-[#a265ff]" />
+          <div className="absolute w-[143px] top-[78px] left-[-113px] -rotate-90 [font-family:'Days_One',Helvetica] font-normal text-[#a265ff] text-base text-center tracking-[0] leading-[15.9px]">COACHING</div>
+          <div className="absolute -top-[-30px] left-[-800px] flex items-center">
+            <div className="w-[13px] h-[13px] bg-[#a265ff] rotate-45"></div>
+            <div className="h-[3px] w-[733px] bg-[#a265ff] -ml-px"></div>
+          </div>
+          <ul className="flex flex-col gap-4">
           {footerLinks.map((link, index) => (
             <li key={index}>
               <Button variant="link" className="p-0 h-auto [font-family:'Poppins',Helvetica] font-medium text-white text-sm tracking-[0] leading-[normal]">{link}</Button>
